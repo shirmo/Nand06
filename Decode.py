76ba4402c21b23283624eff62ba855d898990770
meshi <meshi.djerassi@mail.huji.ac.il>
2019-11-14 17:31:54 +0200

    decode

--- Decode.py
+++ Decode.py
@@ -1,19 +1,20 @@
 import Global_vars as gb
-from Global_vars import *
 
 
-def decode(str):
+def decode(inst):
     # recieves one legal line and translates it to binary
-    if str[0] == '@':
-        if str[1:].isdigit():
-            num = int(str[1:])
+    if inst[0] == '@':
+        if inst[1:].isdigit():
+            num = int(inst[1:])
         else:
-            if str[1:] not in symbol_dict:
-                symbol_dict[str[1:]] = gb.dict_count
+            if inst[1:] not in gb.symbol_dict:
+                gb.symbol_dict[inst[1:]] = gb.dict_count
                 gb.dict_count = gb.dict_count + 1
-            num = symbol_dict[str[1:]]
+            num = gb.symbol_dict[inst[1:]]
 
         return format(num, 'b').zfill(16)
     bin_inst = "111"
-
-
+    if "=" in inst:
+        dest = inst.split("=")[0]
+    else:
+        dest = "null"
