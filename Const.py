6de2337e08487292cff1410c2dcaee1b027ac281
meshi <meshi.djerassi@mail.huji.ac.il>
2019-11-14 16:59:42 +0200

    symbol_dict updated

--- Const.py
+++ Const.py
@@ -1,2 +1,2 @@
 symbol_dict = {"R" + str(i): i for i in range(16)}
-symbol_dict.update({"SCREEN":16384, "KBD":24576})
+symbol_dict.update({"SCREEN": 16384, "KBD": 24576, "SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4})
