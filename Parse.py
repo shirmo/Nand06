e518528edde8c4847aa45e05f208405a84c002f5
shirmo <shirmolina@gmail.com>
2019-11-14 18:04:36 +0200

    Parse file checked and tested

--- Parse.py
+++ Parse.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import Global_vars as gb
 CO = {
     "C": [1],
@@ -8,11 +11,14 @@ CLEAN_LINES = []
 START = "("
 END = ")"
 COMMENT = "//"
+NEW_LINE = "\n"
+TAB = "\t"
 
 
 def lineHandler(line):
-    line.strip()
-
+    line = line.strip(NEW_LINE)
+    line = line.strip(TAB)
+    line = line.strip(" ")
     for i in range(len(line) - 1):
         if line[i] + line[i + 1] == COMMENT:
             line = line[:i]
@@ -24,6 +30,7 @@ def lineHandler(line):
     if line[0] == START and line[len(line) - 1] == END:
         line = line[1:len(line) - 1]
         gb.symbol_dict[line] = len(CLEAN_LINES)
+        return
     CLEAN_LINES.append(line)
     return
 
@@ -37,3 +44,16 @@ def fileReader(file):
             line = fp.readline()
             cnt += 1
         fp.close()
+
+
+def main():
+   filepath = sys.argv[1]
+   if not os.path.isfile(filepath):
+       sys.exit()
+   fileReader(filepath)
+   for i in CLEAN_LINES:
+       print(i)
+
+
+if __name__ == "__main__":
+    main()
