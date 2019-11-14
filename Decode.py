5fd16c00c7586145d61ed0eb6eed741dbb379faa
meshi <meshi.djerassi@mail.huji.ac.il>
2019-11-14 17:13:06 +0200

    rename global_vars and add counter to it

--- Decode.py
+++ Decode.py
@@ -1,4 +1,5 @@
-dict_count = 16
+import Global_vars as gb
+from Global_vars import *
 
 
 def decode(str):
@@ -7,9 +8,10 @@ def decode(str):
         if str[1:].isdigit():
             num = int(str[1:])
         else:
-            if str[1:] not in var_dict:
-                var_dict[str[1:]] = dict_count
-                dict_count = dict_count + 1
-            num = var_dict[str[1:]]
+            if str[1:] not in symbol_dict:
+                symbol_dict[str[1:]] = gb.dict_count
+                gb.dict_count = gb.dict_count + 1
+            num = symbol_dict[str[1:]]
 
         return format(num, 'b').zfill(16)
+    bin_inst = "111"
--- Global_vars.py
+++ Global_vars.py
@@ -1,2 +1,3 @@
 symbol_dict = {"R" + str(i): i for i in range(16)}
 symbol_dict.update({"SCREEN": 16384, "KBD": 24576, "SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4})
+dict_count = 16
