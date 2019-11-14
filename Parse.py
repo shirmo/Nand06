45497c97c27eeb0b8aac13b7a155420d3729bdcc
shirmo <shirmolina@gmail.com>
2019-11-14 19:21:41 +0200

    End after testing, needs to be documented

--- Parse.py
+++ Parse.py
@@ -3,12 +3,11 @@ import Global_vars as gb
 def lineHandler(line):
     line = line.strip(gb.NEW_LINE)
     line = line.strip(gb.TAB)
-    line = line.strip(" ")
     for i in range(len(line) - 1):
         if line[i] + line[i + 1] == gb.COMMENT:
             line = line[:i]
             break
-    line = line.strip(" ")
+    line = line.replace(" ","")
     if len(line) == 0:
         return
 
