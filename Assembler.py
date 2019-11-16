657d45dd089c636c7c9f17a88e1ea714230f0f10
shirmo <shirmolina@gmail.com>
2019-11-16 11:28:18 +0200

    End after testing, needs to be documented

--- Assembler.py
+++ Assembler.py
@@ -6,6 +6,9 @@ import Parse as pr
 import Global_vars as gb
 
 if __name__ == "__main__":
+    """
+    Main function, operates the varies components of the program and connects between them.
+    """
     filepath = sys.argv[1]
     if not os.path.isfile(filepath):
         sys.exit()
@@ -14,6 +17,6 @@ if __name__ == "__main__":
     new_file = filepath.strip("asm") + "hack"
     f = open(new_file, "a+")
     for inst in gb.CLEAN_LINES:
-        line = dc.decode(inst)+gb.NEW_LINE
+        line = dc.decode(inst) + gb.NEW_LINE
         f.write(line)
     f.close()
--- Parse.py
+++ Parse.py
@@ -1,6 +1,12 @@
 import Global_vars as gb
 
+
 def lineHandler(line):
+    """
+    receives a string, removes all white spaces + allocates memory space to symbols
+    :param line: a string of a line from the given asm file
+    :return: void
+    """
     line = line.strip(gb.NEW_LINE)
     line = line.strip(gb.TAB)
     for i in range(len(line) - 1):
@@ -13,14 +19,18 @@ def lineHandler(line):
 
     if line[0] == gb.START and line[len(line) - 1] == gb.END:
         line = line[1:len(line) - 1]
-        gb.symbol_dict[line] = len(gb.CLEAN_LINES)
+        gb.symbol_dict[line] = len(gb.CLEAN_LINES)  # Symbols memory allocation
         return
-
     gb.CLEAN_LINES.append(line)
     return
 
 
 def fileReader(file):
+    """
+    Opens the given file, reads line after line and sends it to lineHandler method
+    :param file: the path of the given file from argv[1]
+    :return: void
+    """
     with open(file) as fp:
         line = fp.readline()
         cnt = 1
